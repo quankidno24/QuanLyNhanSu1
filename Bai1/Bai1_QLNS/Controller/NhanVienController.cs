4aa502dc21abc062c14b7db1c4dce9ec7a24be0f
Quan Nguyen <quankidno24@gmail.com>
2017-05-30 14:40:21 +0700

    Final
    Final

--- Bai1/Bai1_QLNS/Controller/NhanVienController.cs
+++ Bai1/Bai1_QLNS/Controller/NhanVienController.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using Bai1_QLNS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_QLNS.Controller
+{
+    class NhanVienController:Core.ControllerBase<NhanVien>
+    {
+        public DataTable GetDataTable()
+        {
+            string query = "select * from ViewAllNV";
+            return ToDataTable(query);
+        }
+        public NhanVien GetNVByID(int MaNV)
+        {
+            return GetAllData().Find(x => x.MaNV == MaNV);
+        }
+    }
+}
---
+++ Bai1/Bai1_QLNS/Core/ModelBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Core
+{
+    public abstract class ModelBase
+    {
+        public static List<string> FieldNames = null;
+        static Type objType;
+        public abstract Type GetDataType();
+        protected int MaxFields;
+        public object[] ValueData;
+
+        public ModelBase()
+        {
+            Setup();
+            #region tối ưu
+            //if (FieldNames == null)
+            //{
+            //    objType = GetDataType();
+            //    FieldNames = new List<string>();
+            //    PropertyInfo[] Fields = objType.GetProperties();
+            //    foreach (PropertyInfo item in Fields)
+            //    {
+            //        FieldNames.Add(item.Name);
+            //    }
+            //}
+            #endregion
+            objType = GetDataType();
+            FieldNames = new List<string>();
+            PropertyInfo[] Fields = objType.GetProperties();
+            foreach (PropertyInfo item in Fields)
+            {
+                FieldNames.Add(item.Name);
+            }
+        }
+        public abstract void Setup();
+        public void SetData(int pos, object val)
+        {
+            ValueData[pos] = val;
+        }
+        public int GetINT(int pos)
+        {
+            return (int)ValueData[pos];
+        }
+        public string GetSTR(int pos)
+        {
+            return ValueData[pos].ToString();
+        }
+        public double GetDOU(int pos)
+        {
+            return double.Parse(ValueData[pos].ToString());
+        }
+        public float GetFLO(int pos)
+        {
+            return float.Parse(ValueData[pos].ToString());
+        }
+        public DateTime GetDateTime(int pos)
+        {
+            return DateTime.Parse(ValueData[pos].ToString());
+        }
+    }
+}
