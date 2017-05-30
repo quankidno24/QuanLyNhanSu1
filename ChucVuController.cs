417d2b74bd5bf822918f7bd80b1b875f19ddb281
TrangPisi <maitrang6196@gmail.com>
2017-05-30 17:32:28 +0700

    Chuc vu,phong ban,trinh do hoc van
    Chức vụ , phòng ban, trình độ học vấn

---
+++ ChucVuController.cs
@@ -0,0 +1,14 @@
+﻿using Bai1_QLNS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_QLNS.Controller
+{
+    public class ChucVuController:Core.ControllerBase<ChucVu>
+    {
+
+    }
+}
